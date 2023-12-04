11f84d08dd887db350ed0a138a860d3737ad67a4
Lukas Gimberis <lukasgimberis@gmail.com>
2023-12-04 17:11:58 +0000

    Adjust source to match second part of problem

--- src/day_01_a.ts
+++ src/day_01_a.ts
@@ -19,9 +19,35 @@ Adding these together produces 142.
 
 */
 
+function parseMatchedNumber(match: string): number {
+    switch (match) {
+        case "one":
+            return 1
+        case "two":
+            return 2
+        case "three":
+            return 3
+        case "four":
+            return 4
+        case "five":
+            return 5
+        case "six":
+            return 6
+        case "seven":
+            return 7
+        case "eight":
+            return 8
+        case "nine":
+            return 9
+        default:
+            return parseInt(match);
+    }
+}
+
 export function parseString(inputString: string): number {
-    const forwardRe = /^.*?(\d)/mg;  // m - multiline, to grab the first number on every line
-    const backwardRe = /.*(\d).*$/mg;
+    const acceptableCharacters = '\\d|one|two|three|four|five|six|seven|eight|nine'
+    const forwardRe = new RegExp(`^.*?(${acceptableCharacters})`, 'mg');  // m - multiline, to grab the first number on every line
+    const backwardRe = new RegExp(`.*(${acceptableCharacters}).*$`, 'mg');
 
     let forwardMatch = inputString.matchAll(forwardRe);
     let backwardMatch = inputString.matchAll(backwardRe);
@@ -29,12 +55,12 @@ export function parseString(inputString: string): number {
     let sum = 0;
     for (let firstNumber of forwardMatch)
     {
-        sum += 10 * parseInt(firstNumber[1]);
+        sum += 10 * parseMatchedNumber(firstNumber[1]);
     }
 
     for (let lastNumber of backwardMatch)
     {
-        sum += parseInt(lastNumber[1]);
+        sum += parseMatchedNumber(lastNumber[1]);
     }
 
     return sum;
