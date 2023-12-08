462791b45db6a83dcd6ebdf18de3cfbaf2ff8fd3
Lukas Gimberis <lukasgimberis@gmail.com>
2023-12-08 13:34:53 +0000

    Add initial solution d6p1

--- src/day_06/day_06.ts
+++ src/day_06/day_06.ts
@@ -1,6 +1,33 @@
 
-export function productOfRecordBreakingPlays(input: string) {
-    return 0;
+function winningPlaysWithinTime(time: string, distance: string): number {
+    let timeNum = parseInt(time)
+    let distanceNum = parseInt(distance)
+    
+    let winningPlays = 0;
+    for (let i = 0; i <= timeNum; i++) {
+        if (i * (timeNum - i) > distanceNum) {
+            winningPlays++;
+        }
+    }
+    return winningPlays
+}
+
+export function productOfRecordBreakingPlays(input: string): number {
+    let lines = input.split("\n");
+    if (lines.length < 2) return -1;
+
+    let times = lines[0].matchAll(/\d+/g)
+    let distances = lines[1].matchAll(/\d+/g)
+    if (!times || !distances) return -1;
+
+    let time;
+    let distance;
+    let product = 1;
+    while ((time = times.next()) !== null && (distance = distances.next()) !== null && !time.done && !distance.done) {
+        if (time.value[0] && distance.value[0])
+        product *= winningPlaysWithinTime(time.value[0], distance.value[0]);
+    }
+    return product;
 }
 
 function main(data: string)
