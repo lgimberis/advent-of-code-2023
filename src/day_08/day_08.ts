0bba3eda3b26e6b81f38816658b1c1ccb62fa688
Lukas Gimberis <lukasgimberis@gmail.com>
2023-12-08 17:24:53 +0000

    Add d8p1 solution

--- src/day_08/day_08.ts
+++ src/day_08/day_08.ts
@@ -1,5 +1,40 @@
+interface Node {
+  L: string;
+  R: string;
+}
+
+function interpretInput(input: string): {
+  instructions: string;
+  nodes: { [key: string]: Node };
+} {
+  let lines = input.split("\n");
+  if (lines.length < 3) throw Error();
+
+  let instructions = lines[0];
+
+  let nodes: { [key: string]: Node } = {};
+  for (let i = 0; i < lines.length; i++) {
+    let match = lines[i].match(/(\w+) = \((\w+), (\w+)\)/);
+    if (!match || match.length < 4) continue;
+
+    let key = match[1];
+    let node = { L: match[2], R: match[3] };
+    nodes[key] = node;
+  }
+
+  return { instructions, nodes };
+}
+
 export function getStepsToReach(input: string, destination: string): number {
-  return 0;
+  let { instructions, nodes } = interpretInput(input);
+  
+  let currentNode = 'AAA';
+  let stepsTaken = 0;
+  while (currentNode !== destination) {
+    currentNode = nodes[currentNode][instructions[stepsTaken % instructions.length]];
+    stepsTaken++;
+  }
+  return stepsTaken;
 }
 
 function main(data: string) {
