bba5e236af23210b0b2b62257e4cfa4ebabc9cd4
Lukas Gimberis <lukasgimberis@gmail.com>
2023-12-09 16:10:58 +0000

    Add day 9 skeleton

---
+++ src/day_09/day_09.ts
@@ -0,0 +1,15 @@
+function main(data: string) {
+  console.log(data);
+}
+
+if (require.main === module) {
+  const fs = require("node:fs");
+
+  fs.readFile("./day_09_data.txt", "utf8", (err, data) => {
+      if (err) {
+          console.log(err);
+          return;
+      }
+      main(data);
+  });
+}
