02a2b106ef39631cbd1cdbd63fa25ad597fe0deb
Lukas Gimberis <lukasgimberis@gmail.com>
2023-12-15 08:40:57 +0000

    Add d15p2 solution

--- src/day_15/day_15.ts
+++ src/day_15/day_15.ts
@@ -23,6 +23,44 @@ export function sumOfHashes(input: string): number {
 
 export function totalFocusingPower(input: string): number {
   let sum = 0;
+  let boxes: Record<number, {label: string, focalLength: number}[]> = {}
+  for (let line of input.split("\n")) {
+    for (let word of line.split(",")) {
+      if (word.includes("-")) {
+        let label = word.slice(0, word.length - 1);
+        let boxNumber = hash(label);
+        // Go to the relevant box and remove the lens with that label if present in the box
+        if (boxes.hasOwnProperty(boxNumber)) {
+          let index = boxes[boxNumber].findIndex(lens => lens.label == label);
+          if (index != -1) {
+            boxes[boxNumber].splice(index, 1);
+          }
+        }
+      }
+      if (word.includes("=")) {
+        let label = word.slice(0, word.length - 2);
+        let boxNumber = hash(label);
+        let focalLength = parseInt(word[word.length - 1]);
+
+        if (!boxes.hasOwnProperty(boxNumber)) boxes[boxNumber] = [];
+        
+        let index = boxes[boxNumber].findIndex(lens => lens.label == label);
+        if (index == -1) {
+          // If there is no lens in the box with that label, add the lens to the box behind any labels in there
+          boxes[boxNumber].push({label, focalLength});
+        } else {
+          // If there is a lens in the box with the same label, replace it
+          boxes[boxNumber][index] = {label, focalLength};
+        }
+
+      }
+    }
+  }
+  for (let [boxNumber, lenses] of Object.entries(boxes)) {
+    for (let [lensIndex, lens] of lenses.entries()) {
+      sum += (parseInt(boxNumber) + 1) * (lensIndex + 1) * lens.focalLength;
+    }
+  }
   return sum;
 }
 
