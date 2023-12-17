25b13300ebfa056f76b60d3c60c689231cdd571e
Lukas Gimberis <lukasgimberis@gmail.com>
2023-12-17 16:49:09 +0000

    Add some more d17p1 tests

--- src/day_17/day_17.test.ts
+++ src/day_17/day_17.test.ts
@@ -1,5 +1,21 @@
 import { leastHeatLoss } from "./day_17";
 
+test.each([
+    ["11", 2],
+    ["141\n111", 4],
+    ["111\n141", 4],
+    [`
+    0000
+    4404
+    4404
+    4004
+    4040
+    4000
+    `, 0]
+])("simple tests %s -> %d", (input, expected) => {
+    expect(leastHeatLoss(input)).toBe(expected);
+});
+
 let sampleInput = `2413432311323
 3215453535623
 3255245654254
